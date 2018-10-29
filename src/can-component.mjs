d0c51499ca2f977442eef1c039d24e6db0a366ca
Frank Lemanschik <frank@dspeed.eu>
2018-10-29 13:26:04 +0100

    up

--- src/can-component.mjs
+++ src/can-component.mjs
@@ -1,4 +1,4 @@
 //export {default, default as Component } from "can-component";
 import Component from "can-component";
 Component.create = Component.extend;
-export default Component
+export { Component, Component as default }
